f8158b43e3f287477a304caf11cd219c367ae17c
Leo Yulin Li <LeoYulinLi@gmail.com>
2016-05-16 10:32:37 -0700

    update by stephen

---
+++ src/com/github/javaccsf2016/drawboard/ControlPanel.java
@@ -0,0 +1,69 @@
+package com.github.javaccsf2016.drawboard;
+
+
+/* 
+ * This file provides a GUI shell for you to edit. 
+ * Missing code marked with ??? must be replaced by you.
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ControlPanel extends JPanel { 
+
+	private JButton redPenButton;
+	private JButton bluePenButton;
+	private JButton greenPenButton;
+	private JButton eraserButton;
+	private JButton clearAllButton;
+	private DrawBoardDisplay drawingPanel;
+
+	public ControlPanel(DrawBoardDisplay drawingPanel)  {
+		// set the background color
+		setBackground(Color.cyan);
+		
+		this.drawingPanel = drawingPanel;		
+		
+		redPenButton = new JButton("Red Pen");
+		this.add(redPenButton);
+		redPenButton.addActionListener(new ButtonListener());
+	
+		bluePenButton = new JButton("Blue Pen");
+		this.add(bluePenButton);
+		bluePenButton.addActionListener(new ButtonListener());
+		
+		greenPenButton = new JButton("Green Pen");
+		this.add(greenPenButton);
+		greenPenButton.addActionListener(new ButtonListener());
+		
+		eraserButton = new JButton("Eraser");
+		this.add(eraserButton);
+		eraserButton.addActionListener(new ButtonListener());
+		
+		clearAllButton = new JButton("Clear All");
+		this.add(clearAllButton);
+		clearAllButton.addActionListener(new ButtonListener());
+	}
+	
+	
+
+
+	private class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {				
+			if(event.getSource()==redPenButton) {
+				drawingPanel.redPen(); } 
+			else if(event.getSource()==bluePenButton) {
+				drawingPanel.bluePen();} 
+			else if (event.getSource()==greenPenButton){
+					drawingPanel.greenPen();}					
+			else if(event.getSource()==eraserButton) {
+				drawingPanel.eraser();} 
+			else if(event.getSource()==clearAllButton) {
+				drawingPanel.clearAll();}
+		}
+	}
+}
+
+	
+
--- src/com/github/javaccsf2016/drawboard/DrawBoardDisplay.java
+++ src/com/github/javaccsf2016/drawboard/DrawBoardDisplay.java
@@ -1,5 +1,72 @@
 package com.github.javaccsf2016.drawboard;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
 
-public class DrawBoardDisplay {
+
+public class DrawBoardDisplay extends JPanel {
+	private static Color color = Color.black;
+	private ArrayList<Point> pointList;
+	public DrawBoardDisplay() {
+			setBackground(Color.PINK);
+			this.addMouseListener(new MouseMovementListener());			
+			pointList = new ArrayList<>();
+
+			}
+			
+	public static void main(String args[]) {
+		EventQueue.invokeLater(new Runnable() {
+		public void run() {
+				JFrame frame = new JFrame("Draw On Me");
+				frame.setSize(800,600);
+				DrawBoardDisplay drawingPanel = new DrawBoardDisplay();
+				ControlPanel controlPanel = new ControlPanel(drawingPanel);
+				frame.getContentPane().add(drawingPanel, BorderLayout.CENTER);
+				frame.getContentPane().add(controlPanel, BorderLayout.SOUTH);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		   });
+	}
+
+	public class MouseMovementListener extends MouseAdapter{
+		@Override
+		public void mouseMoved(MouseEvent event) {
+			Point mostRecentPosition = event.getPoint();
+			pointList.add(mostRecentPosition);
+			repaint();
+			
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics pen) {
+		super.paintComponent(pen);
+		pen.setColor(color.black);
+
+		for(Point p : pointList) {
+			int x = (int) p.getX();
+			int y = (int) p.getY();
+			pen.drawOval((int)x, (int)y, 100, 100);
+			repaint();
+			
+		}
+	}
+
+	public void redPen() {
+		color = Color.RED; 	}
+
+	public void bluePen() {
+		color = Color.BLUE;	}
+
+	public void greenPen() {
+		color = Color.GREEN;	}
+
+	public void eraser() {
+		color = Color.PINK;	}
+
+	public void clearAll() {
+		pointList.clear();}
 
 }
